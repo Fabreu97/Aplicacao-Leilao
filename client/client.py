acf6813a75838a6caf9d66e4057f029ad6bbe41e
Fernando Abreu <fabreu.1997@alunos.utfpr.edu.br>
2025-09-07 11:16:41 -0300

    changed: unit test for lance microservice

--- client/client.py
+++ client/client.py
@@ -5,7 +5,47 @@ import datetime
 import time
 import json
 import uuid
+import base64
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+
+def lance(client: dict):
+    # ID_leilao
+    # ID_usuario
+    # lance
+    # assinatura
+    time.sleep(10)
+    broker_IP = 'localhost'
+    address = rabbit.ConnectionParameters(broker_IP)
+    connection = rabbit.BlockingConnection(address)
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='direct_lance', exchange_type='direct')
+
+    package = {}
+    package['ID_leilao'] = str(input("Digite o ID do Leilao: "))
+    package['ID_usuario'] = client['ID']
+    package['lance'] = str(input("Digite o valor do lance: "))
+
+    msg = json.dumps(package, sort_keys=True).encode('utf-8')
+    
+    key = RSA.import_key(open(f"./keys/{client['ID']}/private_key.der").read())
+    h = SHA256.new(msg)
+    signature = pkcs1_15.new(key).sign(h)
+
+    package['assinatura'] = base64.b64encode(signature).decode('utf-8')
+
+    message = json.dumps(package, sort_keys=True)
+
+    channel.basic_publish(
+        exchange='direct_lance',
+        routing_key='lance_realizado',
+        body=message
+    )
+    print(f"Message: {message}")
+    connection.close()
+
 
 def getCustomerData() -> dict:
     client = {}
@@ -63,26 +103,36 @@ def envio_leilao(client: dict):
     # Construção do pacote para requisição
     package = {}
     data_inicio = datetime.datetime.now()
-    data_fim = data_inicio + datetime.timedelta(hours=2)
-    package['name'] = client['name']
+    data_fim = data_inicio + datetime.timedelta(minutes=1)
+    package['nome'] = f"Leilao do {client['name']}"
     package['descricao'] = 'Leilao do meu coração'
     package['data_inicio'] = data_inicio.strftime("%d/%m/%Y %H:%M:%S")
     package['data_fim'] = data_fim.strftime("%d/%m/%Y %H:%M:%S")
-    request = json.dumps(package)
-
+    """
+    request = json.dumps(package).encode('utf-8')
+    
+    key = RSA.import_key(open('./keys/{client['ID']}/private_key.der').read())
+    h = SHA256.new(request)
+    signature: bytes = pkcs1_15.new(key).sign(h)
+    package['assinatura'] = signature.hex()
+    """
+    
+    message = json.dumps(package, sort_keys=True)
     # declarar as Exchange
     channel.exchange_declare(exchange='direct_leilao', exchange_type='direct')
 
     channel.basic_publish(
         exchange='direct_leilao',
         routing_key='solicitacao_leilao',
-        body=request
+        body=message
     )
     connection.close()
 
 
 def callback_leilao_inicializado(ch, method, properties, body: bytes):
-    print(body.decode('utf-8'))
+    msg = json.loads(body.decode('utf-8'))
+    for key, value in msg.items():
+        print(f"{key}: {value}")
 
 def main(client: dict):
     broker_IP = 'localhost'
@@ -120,14 +170,15 @@ if __name__ == '__main__':
 
         t1 = threading.Thread(target=main, args=(client,))
         t2 = threading.Thread(target=envio_leilao, args=(client,))
+        t3 = threading.Thread(target=lance, args=(client, ))
         t1.start()
         t2.start()
+        t3.start()
         t1.join()
         t2.join()
+        t3.join()
     except KeyboardInterrupt:
         try:
             sys.exit(0)
         except SystemExit:
             os._exit(0)
-
-
